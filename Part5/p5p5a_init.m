1fbc23c5b6c128bf00b0d28340c76dd75a37af29
Erlend Evenstad Dahlen <erlended@ntnu.no>
2018-10-30 16:21:01 +0100

    added theoretical derivation of discrete matrices

--- Part5/p5p5a_init.m
+++ Part5/p5p5a_init.m
@@ -9,6 +9,8 @@ K = 0.1561;
 T = 72.4347;
 sigma = sqrt(7.919*10^-4);
 K_w = 2*lambda*omega_0*sigma;
+I = eye(5); %identity matrix
+t = 0.1;
 
 % Declaring matrix A, B, C amd E from 5.4 a)
 
@@ -32,9 +34,18 @@ E= [0 0;
     0 1];
 %Discretize continous model using exact discretization
 %Sample frequency of 10Hz-> sample time of 0.1s
+%Using matlab function c2d
+%{
 T_s=0.1;
 [A_d, B_d]=c2d(A,B,T_s);
 [A_d, E_d]=c2d(A,E,T_s);
 C_d=C;
+%}
+%Computing with theory
+A_d = expm(A.*t); % e^A
+syms x;
+f = expm(A.*x);
+B_d = vpaintegral(f,[0 t])*B;
+E_d = vpaintegral(f,[0 t])*E;
 
 
