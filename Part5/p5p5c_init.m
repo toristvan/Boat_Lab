7b29d314315bb823e742bcd29057f1778be2fa63
Tor Istvan Stadler Kjetså <tikjetsa@ntnu.no>
2018-11-06 10:02:25 +0100

    add comments on matrices

--- Part5/p5p5c_init.m
+++ Part5/p5p5c_init.m
@@ -1,11 +1,13 @@
-%Task 5.3 c
+%Task 5.5 c
+%Initialize 5.5 a and b first
 %sim('p5p3c.mdl')
 %Declaring variables
 K=0.1561;
 T=72.4347;
 
 %Declaring matrices
-%w=current; measurement noise
+%w_b=current; measurement noise
+%w_w=waves;
 %w=process noise
 %w=[w_w;w_b];
 %Q=E[w*w'] is process noise covariance given in rad^2
@@ -19,4 +21,13 @@ P_0=[1 0 0 0 0;
     0 0 0 0 2.5*10^-3];
 %^x_0^- - initial a priori state estimate
 x_0=[0;0;0;0;0];
+%states in x:
+%{
+x=[epsilon(integral of psi_w);
+    psi_w (hi freq comp due wave dist);
+    psi (avg heading);
+    r (rate of change for avg heading;
+    b (bias to rudder angle)];
+%}
+R_rad_d=R_rad/T_s; %0.0016(262)
 
