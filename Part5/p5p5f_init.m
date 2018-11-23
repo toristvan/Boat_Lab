6dbb2689c1a2df3a381eacf2ac4b06225d64bdf5
Tor Istvan Stadler Kjetså <tikjetsa@ntnu.no>
2018-11-23 19:40:00 +0100

    add plots for different Q's and notes for differences

--- Part5/p5p5f_init.m
+++ Part5/p5p5f_init.m
@@ -42,6 +42,14 @@ P_0=[1 0 0 0 0;
 x_0=[0;0;0;0;0];
 R_rad_d=R_rad/T_s; %0.0016(262)
 
+my_colors=[[0,0.4470,0.7410];
+    0.8500,0.3250,0.0980;
+    0.9290,0.6940,0.1250;
+    0.4940,0.1840,0.5560;
+    0.4660,0.6740,0.1880;
+    0.3010,0.7450,0.9330;
+    0.6350,0.0780,0.1840];
+
 data = struct('A_d',A_d,'B_d',B_d,'C_d',C_d,'E_d', E_d, 'Q',Q,'R_rad_d', R_rad_d,'P_0',P_0,'x_0',x_0, 'I', eye(5));
 
 sim('p5p5d.slx')
@@ -58,44 +66,210 @@ psi_est_e=psi_estimated.signals.values(1:5000);
 delta_m_e=rudder_input.signals.values(1:5000);
 bias_est_e=bias_estimated.signals.values(1:5000);
 
+
+%Q tune 1
+Q=[0 0;
+    0 0];
+data.Q=Q;
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_1=compass_course.signals.values(1:5000);
+delta_m_d_f_1=rudder_input.signals.values(1:5000);
+bias_est_d_f_1=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_1=compass_course.signals.values(1:5000);
+psi_est_e_f_1=psi_estimated.signals.values(1:5000);
+delta_m_e_f_1=rudder_input.signals.values(1:5000);
+bias_est_e_f_1=bias_estimated.signals.values(1:5000);
+
+%{
+%Q tune 2
 Q=[30 0;
     0 10^-6];
+data.Q=50*Q;
 
 sim('p5p5d.slx')
 sim('p5p5e_1.slx')
 %5 f
 t=compass_course.time(1:5000);
-psi_m_d_f=compass_course.signals.values(1:5000);
-delta_m_d_f=rudder_input.signals.values(1:5000);
-bias_est_d_f=bias_estimated.signals.values(1:5000);
+psi_m_d_f_2=compass_course.signals.values(1:5000);
+delta_m_d_f_2=rudder_input.signals.values(1:5000);
+bias_est_d_f_2=bias_estimated.signals.values(1:5000);
 
 %5 f
-psi_m_e_f=compass_course.signals.values(1:5000);
-psi_est_e_f=psi_estimated.signals.values(1:5000);
-delta_m_e_f=rudder_input.signals.values(1:5000);
-bias_est_e_f=bias_estimated.signals.values(1:5000);
+psi_m_e_f_2=compass_course.signals.values(1:5000);
+psi_est_e_f_2=psi_estimated.signals.values(1:5000);
+delta_m_e_f_2=rudder_input.signals.values(1:5000);
+bias_est_e_f_2=bias_estimated.signals.values(1:5000);
 
+%Q tune 3 [20 0 0 10^-6] too little change
+Q=[30 0;
+    0 0];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_3=compass_course.signals.values(1:5000);
+delta_m_d_f_3=rudder_input.signals.values(1:5000);
+bias_est_d_f_3=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_3=compass_course.signals.values(1:5000);
+psi_est_e_f_3=psi_estimated.signals.values(1:5000);
+delta_m_e_f_3=rudder_input.signals.values(1:5000);
+bias_est_e_f_3=bias_estimated.signals.values(1:5000);
+
+%Q tune 4
+
+Q=[0 0;
+    0 10^-6];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_4=compass_course.signals.values(1:5000);
+delta_m_d_f_4=rudder_input.signals.values(1:5000);
+bias_est_d_f_4=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_4=compass_course.signals.values(1:5000);
+psi_est_e_f_4=psi_estimated.signals.values(1:5000);
+delta_m_e_f_4=rudder_input.signals.values(1:5000);
+bias_est_e_f_4=bias_estimated.signals.values(1:5000);
+
+%Q tune 5
+Q=[0 0.18125;
+    0.18125 10^-6];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_5=compass_course.signals.values(1:5000);
+delta_m_d_f_5=rudder_input.signals.values(1:5000);
+bias_est_d_f_5=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_5=compass_course.signals.values(1:5000);
+psi_est_e_f_5=psi_estimated.signals.values(1:5000);
+delta_m_e_f_5=rudder_input.signals.values(1:5000);
+bias_est_e_f_5=bias_estimated.signals.values(1:5000);
+
+%Q tune 6
+Q=[50*30 0;
+    0 10^-6];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_6=compass_course.signals.values(1:5000);
+delta_m_d_f_6=rudder_input.signals.values(1:5000);
+bias_est_d_f_6=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_6=compass_course.signals.values(1:5000);
+psi_est_e_f_6=psi_estimated.signals.values(1:5000);
+delta_m_e_f_6=rudder_input.signals.values(1:5000);
+bias_est_e_f_6=bias_estimated.signals.values(1:5000);
+
+%Q tune 7
+Q=[30 0;
+    0 50*10^-6];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_7=compass_course.signals.values(1:5000);
+delta_m_d_f_7=rudder_input.signals.values(1:5000);
+bias_est_d_f_7=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_7=compass_course.signals.values(1:5000);
+psi_est_e_f_7=psi_estimated.signals.values(1:5000);
+delta_m_e_f_7=rudder_input.signals.values(1:5000);
+bias_est_e_f_7=bias_estimated.signals.values(1:5000);
+
+%Q tune 8
+Q=[0 10^-6;
+    10^-6 0];
+data.Q=Q;
+
+sim('p5p5d.slx')
+sim('p5p5e_1.slx')
+
+%5 f
+t=compass_course.time(1:5000);
+psi_m_d_f_8=compass_course.signals.values(1:5000);
+delta_m_d_f_8=rudder_input.signals.values(1:5000);
+bias_est_d_f_8=bias_estimated.signals.values(1:5000);
+
+%5 f
+psi_m_e_f_8=compass_course.signals.values(1:5000);
+psi_est_e_f_8=psi_estimated.signals.values(1:5000);
+delta_m_e_f_8=rudder_input.signals.values(1:5000);
+bias_est_e_f_8=bias_estimated.signals.values(1:5000);
+%}
 %plot d and f
 %\psi
+
 figure;
 hold on;
 title({'\psi with original Q matrix and tuned Q matrix.';'Current disturbance'});
 plot(t, psi_ref*ones(5000,1),'--r');
-plot(t,psi_m_d,'b',t, psi_m_d_f,'--g');
+plot(t,psi_m_d);
+plot(t, psi_m_d_f_1);
+%plot(t, psi_m_d_f_2);
+%plot(t, psi_m_d_f_3);
+%plot(t, psi_m_d_f_4);
+%plot(t, psi_m_d_f_5);
+%plot(t, psi_m_d_f_6);
+%plot(t, psi_m_d_f_7);
+%plot(t, psi_m_d_f_8);
 xlabel('$Time [s]$', 'Interpreter', 'latex');
 ylabel('$Angle [deg]$', 'Interpreter', 'latex');
-legend({'\psi_r','\psi with original Q', '\psi with tuned Q'});
+legend({'\psi_r','\psi with Q_0',...
+    '\psi with Q_1','\psi with Q_2',...
+    '\psi with Q_3','\psi with Q_4',...
+    '\psi with Q_5','\psi with Q_6',...
+    '\psi with Q_7','\psi with Q_8';});
 hold off;
 
 %\delta
 figure;
 hold on;
 title({'\delta with original Q matrix and tuned Q matrix.';'Current disturbance'});
-plot(t,delta_m_d,'b', t, delta_m_d_f,'--g');
-plot(t,bias_est_d,'m',t,bias_est_d_f,'--c');
+plot(t,delta_m_d,t,bias_est_d);
+plot(t, delta_m_d_f_1,t,bias_est_d_f_1);
+%plot(t, delta_m_d_f_2,t,bias_est_d_f_2);
+%plot(t, delta_m_d_f_3,t,bias_est_d_f_3);
+%plot(t, delta_m_d_f_4,t,bias_est_d_f_4);
+%plot(t, delta_m_d_f_5,t,bias_est_d_f_5);
+%plot(t, delta_m_d_f_6,t,bias_est_d_f_6);
+%plot(t, delta_m_d_f_7,t,bias_est_d_f_7);
+%plot(t, delta_m_d_f_8,t,bias_est_d_f_8);
 xlabel('$Time [s]$', 'Interpreter', 'latex');
 ylabel('$Angle [deg]$', 'Interpreter', 'latex');
-legend({'\delta with original Q','\delta with tuned Q','Estimated bias with original Q','Estimated bias with tuned Q'});
+legend({'\delta with Q_0','Estimated bias with Q_0',...
+    '\delta with Q_1','Estimated bias with Q_1',...
+    '\delta with Q_2','Estimated bias with Q_2',...
+    '\delta with Q_3','Estimated bias with Q_3',...
+    '\delta with Q_4','Estimated bias with Q_4',...
+    '\delta with Q_5','Estimated bias with Q_5',...
+    '\delta with Q_6','Estimated bias with Q_6',...
+    '\delta with Q_7','Estimated bias with Q_7',...
+    '\delta with Q_8','Estimated bias with Q_8';});
 hold off;
 
 %plot e and f
@@ -104,20 +278,52 @@ figure;
 hold on;
 title({'\psi with original Q matrix and tuned Q matrix.';'Current and wave disturbance'});
 plot(t, psi_ref*ones(5000,1),'--r');
-plot(t,psi_m_e,'b',t, psi_m_e_f,'--g');
+plot(t,psi_m_e,t, psi_est_e);
+plot(t,psi_m_e_f_1,t,psi_est_e_f_1);
+%plot(t,psi_m_e_f_2,t,psi_est_e_f_2);
+%plot(t,psi_m_e_f_3,t,psi_est_e_f_3);
+%plot(t,psi_m_e_f_4,t,psi_est_e_f_4);
+%plot(t,psi_m_e_f_5,t,psi_est_e_f_5);
+%plot(t,psi_m_e_f_6,t,psi_est_e_f_6);
+%plot(t,psi_m_e_f_7,t,psi_est_e_f_7);
+%plot(t,psi_m_e_f_8,t,psi_est_e_f_8);
 xlabel('$Time [s]$', 'Interpreter', 'latex');
 ylabel('$Angle [deg]$', 'Interpreter', 'latex');
-legend({'\psi_r','\psi with original Q', '\psi with tuned Q'});
+legend({'\psi_r','\psi with original Q', '\psi with tuned Q','Estimated \psi with original Q','Estimated \psi with tuned Q'});
+legend({'\psi_r','\psi with Q_0','estimated \psi with Q_0',...
+    '\psi with Q_1','estimated \psi with Q_1',...
+    '\psi with Q_2','estimated \psi with Q_2',...
+    '\psi with Q_3','estimated \psi with Q_3',...
+    '\psi with Q_4','estimated \psi with Q_4',...
+    '\psi with Q_5','estimated \psi with Q_5',...
+    '\psi with Q_6','estimated \psi with Q_6',...
+    '\psi with Q_7','estimated \psi with Q_7',...
+    '\psi with Q_8','estimated \psi with Q_8';});
 hold off;
 
 %\delta
 figure;
 hold on;
 title({'\delta with original Q matrix and tuned Q matrix.';'Current and wave disturbance'});
-plot(t,delta_m_e,'b', t, delta_m_e_f,'--g');
-plot(t,bias_est_e,'m',t,bias_est_e_f,'--c');
+plot(t,delta_m_e, t, bias_est_e);
+plot(t,delta_m_e_f_1,t,bias_est_e_f_1);
+%plot(t,delta_m_e_f_2,t,bias_est_e_f_2);
+%plot(t,delta_m_e_f_3,t,bias_est_e_f_3);
+%plot(t,delta_m_e_f_4,t,bias_est_e_f_4);
+%plot(t,delta_m_e_f_5,t,bias_est_e_f_5);
+%plot(t,delta_m_e_f_6,t,bias_est_e_f_6);
+%plot(t,delta_m_e_f_7,t,bias_est_e_f_7);
+%plot(t,delta_m_e_f_8,t,bias_est_e_f_8);
 xlabel('$Time [s]$', 'Interpreter', 'latex');
 ylabel('$Angle [deg]$', 'Interpreter', 'latex');
-legend({'\delta with original Q','\delta with tuned Q','Estimated bias with original Q','Estimated bias with tuned Q'});
+legend({'\delta with Q_0','Estimated bias with Q_0',...
+    '\delta with Q_1','Estimated bias with Q_1',...
+    '\delta with Q_2','Estimated bias with Q_2',...
+    '\delta with Q_3','Estimated bias with Q_3',...
+    '\delta with Q_4','Estimated bias with Q_4',...
+    '\delta with Q_5','Estimated bias with Q_5',...
+    '\delta with Q_6','Estimated bias with Q_6',...
+    '\delta with Q_7','Estimated bias with Q_7',...
+    '\delta with Q_8','Estimated bias with Q_8';});
 hold off;
 
